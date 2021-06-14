e8225ebd4c2e801c8b56b51f777676a285ceb63e
oldsalao <bucktoothvillain@gmail.com>
2021-06-15 03:26:43 +0900

    DB 연결 테스트

---
+++ src/main/java/com/haneul/testdb/TestDbServlet.java
@@ -0,0 +1,45 @@
+package com.haneul.testdb;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@WebServlet(name = "TestDbServlet", value = "/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = "springstudent";
+        String pass = "springstudent";
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306?useSSL=false";
+        String driver = "com.mysql.jdbc.Driver";
+
+        try{
+
+            PrintWriter out = response.getWriter();
+
+            out.println("Connecting to database: " + jdbcUrl);
+
+            Class.forName(driver);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl,user,pass);
+
+            out.println("Success!");
+
+            myConn.close();
+
+        }catch (Exception e) {
+            e.printStackTrace();
+            throw new ServletException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
