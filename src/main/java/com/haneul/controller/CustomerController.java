6af890bbfa0242b53a3a88064fff85e8419ba4a3
oldsalao <bucktoothvillain@gmail.com>
2021-06-15 21:59:02 +0900

    Controller class의 listcustomers 메소드 구현

--- src/main/java/com/haneul/controller/CustomerController.java
+++ src/main/java/com/haneul/controller/CustomerController.java
@@ -1,14 +1,31 @@
 package com.haneul.controller;
 
+import com.haneul.dao.CustomerDAO;
+import com.haneul.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    private CustomerDAO customerDAO;
+
+    @Autowired
+    public CustomerController(CustomerDAO customerDAO){
+        this.customerDAO = customerDAO;
+    }
+
     @RequestMapping("/list")
     public String listCustomers(Model theModel){
+
+        List<Customer> customerList = customerDAO.getCustomers();
+
+        theModel.addAttribute("customerList",customerList);
 
         return "list-customers";
     }
