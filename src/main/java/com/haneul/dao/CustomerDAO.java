f1b795b72b686f9ddd990ae2607e41397e162ea9
oldsalao <bucktoothvillain@gmail.com>
2021-06-15 21:52:01 +0900

    DAO 인터페이스, 구현 클래스 추가

---
+++ src/main/java/com/haneul/dao/CustomerDAO.java
@@ -0,0 +1,9 @@
+package com.haneul.dao;
+
+import com.haneul.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+    public List<Customer> getCustomers();
+}
---
+++ src/main/java/com/haneul/dao/CustomerDAOImpl.java
@@ -0,0 +1,37 @@
+package com.haneul.dao;
+
+import com.haneul.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO{
+
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public CustomerDAOImpl(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        Query<Customer> theQuery = null;
+        List<Customer> customerList=null;
+
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        theQuery = currentSession.createQuery("from Customer",Customer.class);
+
+        customerList = theQuery.getResultList();
+
+        return customerList;
+    }
+}
